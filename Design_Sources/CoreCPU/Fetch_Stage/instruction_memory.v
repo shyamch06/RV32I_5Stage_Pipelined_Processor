17cb9ceefc7fbfef8acb84f14bf23fe5f1873a18
shyamch06 <cherukurishyamsundhar2006@gmail.com>
2026-09-21 10:52:21 +0530

    RV32I_Pipelined

--- Design_Sources/CoreCPU/Fetch_Stage/instruction_memory.v
+++ Design_Sources/CoreCPU/Fetch_Stage/instruction_memory.v
@@ -3,8 +3,11 @@ module instruction_memory (
     output [31:0] InstrF
 );
     reg [31:0] mem [127:0];
+    integer i;
     initial begin
-        $readmemh("program.mem",mem);
+        for (i = 0; i < 128; i = i + 1)
+            mem[i] = 32'h00000013; // NOP (ADDI x0, x0, 0) -- safe default
+        $readmemh("program.mem", mem);
     end
     assign InstrF = mem[PCF[31:2]];
 endmodule
